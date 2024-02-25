a4c3387550d93ed0197aea2609f55a7a1807f18f
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2024-02-25 12:14:40 +0100

    Update AssemblyInfo.vb

--- IniFileControlDemo/My Project/AssemblyInfo.vb
+++ IniFileControlDemo/My Project/AssemblyInfo.vb
@@ -2,11 +2,7 @@ Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' Allgemeine Informationen über eine Assembly werden über die folgenden
-' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-' die einer Assembly zugeordnet sind.
 
-' Werte der Assemblyattribute überprüfen
 
 <Assembly: AssemblyTitle("IniFileControlDemo")>
 <Assembly: AssemblyDescription("")>
@@ -17,28 +13,14 @@ Imports System.Runtime.InteropServices
 
 <Assembly: ComVisible(False)>
 
-'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+
 <Assembly: Guid("a3911736-8f42-448f-87a9-4991dcf65e3b")>
 
-' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-'
-'      Hauptversion
-'      Nebenversion
-'      Buildnummer
-'      Revision
-'
-' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-' indem Sie "*" wie unten gezeigt eingeben:
-' <Assembly: AssemblyVersion("1.0.*")>
 
-<<<<<<< HEAD
+
+
+
 <Assembly: AssemblyVersion("1.2024.0224.61")>
 <Assembly: AssemblyFileVersion("1.2024.0224.61")>
 
 <assembly: AssemblyInformationalVersion("0.2024.0224.61")>
-=======
-<Assembly: AssemblyVersion("1.2024.0224.59")>
-<Assembly: AssemblyFileVersion("1.2024.0224.59")>
-
-<assembly: AssemblyInformationalVersion("0.2024.0224.59")>
->>>>>>> 1f3e76f3982fc685393c0ebad81401f9fe01a96c
